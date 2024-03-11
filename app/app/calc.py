c3c77b8eccab80d5880e0f58234c0bd12f81f7ea
lnvazquezmartinezait <lisandrovazquezait@gmail.com>
2024-03-11 14:07:38 -0300

    TESTS

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator functions
+"""
+
+def add(x, y):
+    """add x and y and return result"""
+    return x + y
+
+def subtract(x, y):
+    """sibtract x from y"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    """test the calc module"""
+    def test_add_nubers(self):
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtract number"""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
